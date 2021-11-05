f918d6cb895b6c6edc09272cd0d22def7e27e4ed
LOLO-BELEU <58961222+Lorraine-Olegario@users.noreply.github.com>
2021-11-05 15:13:18 -0300

    conexão banco dados

---
+++ Modulo_8/conexao_bd/cnx.php
@@ -0,0 +1,7 @@
+<?php
+   $var1_a = 'localhost';
+   $var1_b = 'modulo8';
+   $var1_c = 'root1';
+   $var1_d = '$TcCk21_17!';
+   $conn = new PDO("mysql:host=$var1_a;dbname=$var1_b", $var1_c, $var1_d); 
+?>
